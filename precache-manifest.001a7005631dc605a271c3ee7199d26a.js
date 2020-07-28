da583eddf72e9288096c9ad295d681dc78ee673f
omerHamid <66900959+omerHamid@users.noreply.github.com>
2020-07-28 18:20:21 +0530

    Updates

---
+++ precache-manifest.001a7005631dc605a271c3ee7199d26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7966630b63c53cdaf644bf501997d99",
+    "url": "/hello-robot/index.html"
+  },
+  {
+    "revision": "9db31370247db2a054c2",
+    "url": "/hello-robot/static/css/main.15e6bf5c.chunk.css"
+  },
+  {
+    "revision": "d00e54cd8dac045d6351",
+    "url": "/hello-robot/static/js/2.1378fc75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-robot/static/js/2.1378fc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db31370247db2a054c2",
+    "url": "/hello-robot/static/js/main.ab10fbc2.chunk.js"
+  },
+  {
+    "revision": "ff61155740b7e252f694",
+    "url": "/hello-robot/static/js/runtime-main.b2b5330a.js"
+  },
+  {
+    "revision": "f5d3f71ae30487c3dbce7ded51dfb285",
+    "url": "/hello-robot/static/media/JMH.f5d3f71a.ttf"
+  }
+]);
